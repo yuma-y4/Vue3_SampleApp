d461f926c0209b02f03acca118c63aa820d53e19
yuma-y4 <mfnvbbflbgrt5tupni6nijtfpxroff@gmail.com>
2022-10-10 15:52:16 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const app = Vue.createApp({
+  data: () => ({
+    items: null,
+    keyword: "",
+    message: "",
+  }),
+  watch: {
+    keyword: function (newKeyword, oldKeyword) {
+      console.log(newKeyword);
+      this.message = "Waiting for you to stop typing...";
+      this.debouncedGetAnswer();
+    },
+  },
+  mounted: function () {
+    // this.keyword = 'JavaScript'
+    // this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.keyword === "") {
+        console.log("karamoji");
+        this.items = null;
+        return;
+      }
+
+      this.message = "Lading...";
+      const vm = this;
+      const params = { page: 1, per_page: 20, query: this.keyword };
+      axios
+        .get("https://qiita.com/api/v2/items", { params })
+        .then(function (response) {
+          // console.log(response)
+          vm.items = response.data;
+        })
+        .catch(function (error) {
+          vm.message = "Error!" + error;
+        })
+        .finally(function () {
+          vm.message = "";
+        });
+    },
+  },
+});
+app.mount("#app");
